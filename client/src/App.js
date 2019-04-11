e94cfce8c0aef8c17d7ef3579ef15b3befe32fd2
bgatlin <brandongatlin1981@me.com>
2019-04-11 15:00:18 -0500

    added button counter with useState

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect }from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = (props)=> {
+
+  const [count, setCount] = useState(0);
+  const [topPos, setTop] = useState(0);
+
+  const divStyle = {
+    margin: topPos,
+  };
+
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  // useEffect(() => {
+  //   let div = document.getElementById('count');
+  //   div.style.marginTop = topPost + "px"
+  // });
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={divStyle} className="App">
+        <p id="count">{count}</p>
+        <button onClick={()=> {
+          setCount(count + 1);
+          setTop(topPos + 100);
+        }}>Click Me</button>
       </div>
     );
-  }
 }
 
 export default App;
